17fed3afc4499ddecc66e2132ff5663f4a573a67
pkirti33 <89867716+pkirti33@users.noreply.github.com>
2024-05-25 03:34:26 +0000

    updated setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -4,10 +4,3 @@ if ! command -v rye &> /dev/null
 then
     curl -sSf https://rye.astral.sh/get | bash
 fi
-
-source "$HOME/.rye/env"
-
-rye add -r requirements.lock
-rye add -r requirements-dev.lock
-
-rye sync
